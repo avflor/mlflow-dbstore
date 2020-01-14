4f3087c1c5f440de16c651a139f98ab0bd25cd0c
avflor <avflor@microsoft.com>
2020-01-14 22:38:53 +0000

    updated setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
   name='dbstoreplugin',
   version='1.0.0',
   description='Plugin that provides DB Artifact Store functionality for MLflow',
   author='Avrilia Floratou',
-  author_email='avrilia.floratou@@microsoft.com',
+  author_email='avflor@microsoft.com',
   packages=['dbstoreplugin'],
   install_requires=[
       'mlflow',
