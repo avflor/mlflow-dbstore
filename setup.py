8bb4c3d5d45a88751605b67538acc553ff2b7580
avflor <avflor@microsoft.com>
2020-01-14 23:42:03 +0000

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-  name='dbstoreplugin',
-  version='1.0.0',
-  description='Plugin that provides DB Artifact Store functionality for MLflow',
-  author='Avrilia Floratou',
-  author_email='avflor@microsoft.com',
-  packages=['dbstoreplugin'],
-  install_requires=[
-      'mlflow',
-  ],
-  entry_points={
-      "mlflow.artifact_repository": [
-        "mssql=dbstoreplugin.store.artifact:DBArtifactRepository"
-      ]
-  },
+    name='mlflow-dbstore',
+    version='1.0.0',
+    description='Plugin that provides DB Artifact Store functionality for MLflow',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Avrilia Floratou',
+    author_email='avflor@microsoft.com',
+    url="https://github.com/avflor/mlflow-dbstore",
+    packages=find_packages(),
+    install_requires=[
+        'mlflow',
+    ],
+    entry_points={
+        "mlflow.artifact_repository": [
+            "mssql=dbstoreplugin.store.artifact:DBArtifactRepository"
+        ]
+    },
 )
