fed8c9763f284f1bfb6f8d177bf0c114e0b2ae17
avflor <avflor@microsoft.com>
2020-01-14 22:01:30 +0000

    Added headers to src files

--- dbstoreplugin/store/db/initial_artifact_store_models.py
+++ dbstoreplugin/store/db/initial_artifact_store_models.py
@@ -1,13 +1,8 @@
-# Snapshot of MLflow DB models as of the 0.9.1 release, prior to the first database migration.
-# Used to standardize initial database state.
-# Copied with modifications from
-# https://github.com/mlflow/mlflow/blob/v0.9.1/mlflow/store/dbmodels/models.py, which
-# is the first database schema that users could be running. In particular, modifications have
-# been made to substitute constants from MLflow with hard-coded values (e.g. replacing
-# SourceType.to_string(SourceType.NOTEBOOK) with the constant "NOTEBOOK") and ensure
-# that all constraint names are unique. Note that pre-1.0 database schemas did not have unique
-# constraint names - we provided a one-time migration script for pre-1.0 users so that their
-# database schema matched the schema in this file.
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 
 from sqlalchemy import (
     Column, String, VARBINARY, BigInteger, Integer, PrimaryKeyConstraint)
